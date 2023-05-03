5625aa937e15908d6b265e8a7ba1970c917d7d8d
irbizzelus <117110119+irbizzelus@users.noreply.github.com>
2023-05-03 15:32:15 +0300

    Delete dialogmanager.lua
    Signed-off-by: irbizzelus <117110119+irbizzelus@users.noreply.github.com>

--- lua/dialogmanager.lua
+++
@@ -1,4 +0,0 @@
--- Bain/Locke's hostage kill voice line fix by Dorpenka
-function DialogManager:queue_narrator_dialog(id, params)
-	return self:queue_dialog(self._narrator_prefix .. id, params)
-end
